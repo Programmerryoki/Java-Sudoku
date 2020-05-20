7b481b55e22ec6287e273aac8903b0cd0eca163f
Programmerryoki <ryoki.kunii@sjsu.edu>
2020-05-20 14:44:23 +0900

    Created All the Classes

---
+++ src/app/SudokuApp.java
@@ -0,0 +1,10 @@
+package app;
+
+import controller.MasterController;
+
+public class SudokuApp {
+	public static void main(String[] args) 
+	{
+		new MasterController();
+	}
+}
---
+++ src/model/Sudoku.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Sudoku {
+	public Sudoku()
+	{
+		
+	}
+	
+	public int[][] getGrid() {
+		return grid;
+	}
+
+	public void setGrid(int[][] grid) {
+		this.grid = grid;
+	}
+
+	private int[][] grid;
+}
